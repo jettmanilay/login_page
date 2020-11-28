f73c2ea2b3ee7130325c7aae14e88240d5da1ab6
jettmanilay <rumanilay1@up.edu.ph>
2020-11-28 17:44:32 +0800

    master

---
+++ lib/login_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/homepage.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final emailInput = Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            hintText: 'Username',
+            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(50.0))),
+      ),
+    );
+    final passwordInput = Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: 'Password',
+            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(50.0))),
+      ),
+    );
+    final buttonLogin = Padding(
+      padding: EdgeInsets.only(bottom: 5),
+      child: ButtonTheme(
+        height: 56,
+        child: RaisedButton(
+          child: Text('Login',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+          color: Colors.green,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          onPressed: () => {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ))
+          },
+        ),
+      ),
+    );
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          children: <Widget>[
+            emailInput,
+            passwordInput,
+            buttonLogin,
+          ],
+        ),
+      ),
+    ));
+  }
+}
